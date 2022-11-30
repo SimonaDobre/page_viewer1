8b63310567d0db44b267742449851b5842178ab4
SimonaDobre <simonadobre85@yahoo.com>
2022-11-30 18:34:30 +0200

    update1

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,9 @@ class _MyHomePageState extends State<MyHomePage> {
   final List<String> titlesList = <String>[];
   final List<String> images = <String>[];
 
+  final List<List<String>> genre = <List<String>>[];
+  final List<double>? rating = <double>[];
+
   @override
   void initState() {
     super.initState();
@@ -45,18 +48,20 @@ class _MyHomePageState extends State<MyHomePage> {
   void getMovies() {
     get(Uri.parse('https://yts.mx/api/v2/list_movies.json'))
         .then((Response response) {
-      // response.body;
       final Map<String, dynamic> list1 =
           jsonDecode(response.body) as Map<String, dynamic>;
       final Map<String, dynamic> list2 = list1['data'] as Map<String, dynamic>;
-      final List<dynamic> moviesList = list2['movies'] as List<dynamic>;
+      final List<Map<dynamic, dynamic>> moviesList =
+          List<Map<dynamic, dynamic>>.from(list2['movies'] as List<dynamic>);
 
-      for (final currentMovie in moviesList) {
+      for (int i = 0; i < moviesList.length; i++) {
+        final Map<dynamic, dynamic> currentMovie = moviesList[i];
+
         titlesList.add(currentMovie['title'] as String);
         images.add(currentMovie['medium_cover_image'] as String);
+        genre.add(currentMovie['genres'] as List<String>);
+        rating?.add(currentMovie['rating'] as double);
       }
-
-      moviesList.map((dynamic currentItem) => currentItem['title']);
 
       setState(() {
         isLoading = false;
@@ -67,34 +72,68 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Builder(builder: (context) {
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Builder(
+        builder: (BuildContext context) {
           if (isLoading) {
             return const Center(child: CircularProgressIndicator());
           }
 
           return PageView.builder(
-              itemBuilder: (BuildContext context, int index) {
-            final String currentTitle = titlesList[index];
-            final String currentImage = images[index];
+            itemBuilder: (BuildContext context, int index) {
+              final String currentTitle = titlesList[index];
+              final String currentImage = images[index];
+              final List<String> currentGenreList = genre[index];
+              final double? currentRating = rating?[index];
 
-            return Column(
-              children: <Widget>[
-                Image.network(currentImage),
-                Padding(
-                  padding: const EdgeInsets.all(28),
-                  child: Text(
-                    currentTitle,
-                    style: const TextStyle(
-                      fontSize: 20,
+              return Column(
+                children: <Widget>[
+                  Image.network(currentImage),
+                  Padding(
+                    padding: const EdgeInsets.all(28),
+                    child: Text(
+                      currentTitle,
+                      style: const TextStyle(
+                        fontSize: 20,
+                      ),
                     ),
                   ),
-                )
-              ],
-            );
-          });
-        }));
+                  Text(currentGenreList[0]),
+                  Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(38),
+                        child: Text(
+                          '$currentRating',
+                          style: TextStyle(fontSize: 40),
+                        ),
+                      ), //$currentRating
+                      const Padding(
+                        padding: EdgeInsets.all(38),
+                        child: Text(
+                          '2',
+                          style: TextStyle(fontSize: 40),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(38),
+                        child: Text(
+                          '3',
+                          style: TextStyle(fontSize: 40),
+                        ),
+                      )
+                    ],
+                  ),
+                  ElevatedButton(
+                      onPressed: () {}, child: const Text('Buy Tickets'))
+                ],
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
